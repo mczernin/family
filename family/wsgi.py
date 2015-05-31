1f6895362a10e4b6cbabb744747c08205186f87a
Diane Kaplan <dianekaplan@gmail.com>
2015-05-31 17:45:55 -0400

    updated wsgi.py

--- family/wsgi.py
+++ family/wsgi.py
@@ -9,13 +9,15 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-
-# DK adding 2 lines from heroku/django getting started: 
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "family.settings")
 
 application = get_wsgi_application()
+
+# DK added lines from heroku/django getting started- 
+# come back later to clean up what's redundant
+
+from django.core.wsgi import get_wsgi_application #this one may have been there already
+
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
