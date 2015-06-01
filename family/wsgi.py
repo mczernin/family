4070200be1302c1bb8d62c97d03ee004237b98d1
Diane Kaplan <dianekaplan@gmail.com>
2015-05-31 20:52:01 -0400

    added back import for get_wsgi_application

--- family/wsgi.py
+++ family/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "family.settings")
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
 
